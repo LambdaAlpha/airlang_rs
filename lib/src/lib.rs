75ab10a7a6e5ad8da0b78dd431cd7197efdbcd3d
LambdaAlpha <lambdaalpha@airlang.fun>
2022-09-28 08:55:23 +0800

    add deny rules
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,22 @@
+#![deny(
+    bad_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true
+)]
+
 use val::{Bytes, Val};
 
 pub fn interpret(src: &str) -> String {
