7785c1bd027feb21a3b65973b2867a71a9dacd48
LambdaAlpha <lambdaalpha@airlang.fun>
2023-04-26 17:13:00 +0800

    remove hard-encoded version code and name
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/semantics/prelude/meta.rs
+++ lib/src/semantics/prelude/meta.rs
@@ -7,9 +7,13 @@ use crate::{
 };
 
 pub(crate) fn version_code() -> Val {
-    Val::Int(Int::from_sign_string_radix(true, "3", 10))
+    const MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
+    const MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
+    const PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
+    let version_code = format!("{:0>3}{:0>3}{:0>3}", MAJOR, MINOR, PATCH);
+    Val::Int(Int::from_sign_string_radix(true, &version_code, 10))
 }
 
 pub(crate) fn version_name() -> Val {
-    Val::String(Str::from("0.0.3"))
+    Val::String(Str::from(env!("CARGO_PKG_VERSION")))
 }
