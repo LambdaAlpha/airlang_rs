a1448f995f60a73ff471939638b7096abf0a49ab
LambdaAlpha <lambdaalpha@airlang.fun>
2023-06-10 09:53:10 +0800

    swap syntax of list and wrapper
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/syntax.rs
+++ lib/src/syntax.rs
@@ -20,12 +20,12 @@ const PRESERVE_PREFIX: char = '\'';
 const SEPARATOR: char = ',';
 const PAIR_SEPARATOR: char = ':';
 const REVERSE_SEPARATOR: char = '?';
-const LIST_LEFT: char = '(';
-const LIST_RIGHT: char = ')';
+const LIST_LEFT: char = '[';
+const LIST_RIGHT: char = ']';
 const MAP_LEFT: char = '{';
 const MAP_RIGHT: char = '}';
-const WRAP_LEFT: char = '[';
-const WRAP_RIGHT: char = ']';
+const WRAP_LEFT: char = '(';
+const WRAP_RIGHT: char = ')';
 
 #[derive(Error, Debug)]
 #[error("ParseError:\n{msg}")]
