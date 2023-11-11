ef4a157a01d0c606ce1d5e26978b1d265c66ffd4
LambdaAlpha <lambdaalpha@airlang.fun>
2023-11-11 21:55:24 +0800

    add unit prelude
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/semantics/prelude/unit.rs
+++ lib/src/semantics/prelude/unit.rs
@@ -1,18 +1,41 @@
-use crate::semantics::{
-    ctx::NameMap,
-    prelude::Prelude,
+use crate::{
+    semantics::{
+        ctx::NameMap,
+        eval_mode::EvalMode,
+        input_mode::InputMode,
+        prelude::{
+            named_free_fn,
+            Named,
+            Prelude,
+        },
+        val::FuncVal,
+        Val,
+    },
+    types::Unit,
 };
 
 #[derive(Clone)]
-pub(crate) struct UnitPrelude {}
+pub(crate) struct UnitPrelude {
+    unit: Named<FuncVal>,
+}
 
-#[allow(clippy::derivable_impls)]
 impl Default for UnitPrelude {
     fn default() -> Self {
-        UnitPrelude {}
+        UnitPrelude { unit: unit() }
     }
 }
 
 impl Prelude for UnitPrelude {
-    fn put(&self, _m: &mut NameMap) {}
+    fn put(&self, m: &mut NameMap) {
+        self.unit.put(m);
+    }
+}
+
+fn unit() -> Named<FuncVal> {
+    let input_mode = InputMode::Any(EvalMode::Value);
+    named_free_fn("'", input_mode, fn_unit)
+}
+
+fn fn_unit(_input: Val) -> Val {
+    Val::Unit(Unit)
 }
