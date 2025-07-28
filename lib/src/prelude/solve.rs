03ebd54efa9fa917234587a0266743daf52e1825
LambdaAlpha <lambdaalpha@airlang.fun>
2025-07-28 09:54:46 +0800

    add reverse preludes
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/prelude/solve.rs
+++ lib/src/prelude/solve.rs
@@ -4,35 +4,50 @@ use super::FreeFn;
 use super::Prelude;
 use super::PreludeCtx;
 use super::free_impl;
+use crate::prelude::mode::FuncMode;
+use crate::prelude::mode::SymbolMode;
 use crate::prelude::setup::default_free_mode;
+use crate::prelude::setup::free_mode;
+use crate::semantics::solver::REVERSE_MAP;
 use crate::semantics::solver::SOLVER;
 use crate::semantics::val::FreeStaticPrimFuncVal;
 use crate::semantics::val::Val;
+use crate::type_::Map;
+use crate::type_::Pair;
 
 #[derive(Clone)]
 pub struct SolvePrelude {
-    pub get_solver: FreeStaticPrimFuncVal,
+    pub solver: FreeStaticPrimFuncVal,
     pub set_solver: FreeStaticPrimFuncVal,
+    pub reverse: FreeStaticPrimFuncVal,
+    pub set_reverse: FreeStaticPrimFuncVal,
 }
 
 impl Default for SolvePrelude {
     fn default() -> Self {
-        SolvePrelude { get_solver: get_solver(), set_solver: set_solver() }
+        SolvePrelude {
+            solver: solver(),
+            set_solver: set_solver(),
+            reverse: reverse(),
+            set_reverse: set_reverse(),
+        }
     }
 }
 
 impl Prelude for SolvePrelude {
     fn put(&self, ctx: &mut dyn PreludeCtx) {
-        self.get_solver.put(ctx);
+        self.solver.put(ctx);
         self.set_solver.put(ctx);
+        self.reverse.put(ctx);
+        self.set_reverse.put(ctx);
     }
 }
 
-pub fn get_solver() -> FreeStaticPrimFuncVal {
-    FreeFn { id: "solver!", f: free_impl(fn_get_solver), mode: default_free_mode() }.free_static()
+pub fn solver() -> FreeStaticPrimFuncVal {
+    FreeFn { id: "solver!", f: free_impl(fn_solver), mode: default_free_mode() }.free_static()
 }
 
-fn fn_get_solver(_input: Val) -> Val {
+fn fn_solver(_input: Val) -> Val {
     SOLVER.with(|solver| {
         let Ok(solver) = solver.try_borrow() else {
             return Val::default();
@@ -57,6 +72,70 @@ fn fn_set_solver(input: Val) -> Val {
             return;
         };
         *solver = new_solver;
+    });
+    Val::default()
+}
+
+pub fn reverse() -> FreeStaticPrimFuncVal {
+    FreeFn {
+        id: "reverse!",
+        f: free_impl(fn_reverse),
+        mode: free_mode(FuncMode::symbol_mode(SymbolMode::Literal)),
+    }
+    .free_static()
+}
+
+fn fn_reverse(input: Val) -> Val {
+    let Val::Symbol(id) = input else {
+        error!("input {input:?} should be a symbol");
+        return Val::default();
+    };
+    REVERSE_MAP.with(|map| {
+        let Ok(map) = map.try_borrow() else {
+            error!("reverse map should be readable");
+            return Val::default();
+        };
+        let Some(func) = map.get(&id) else {
+            error!("reverse func of {id:?} should exist");
+            return Val::default();
+        };
+        Val::Func(func.clone())
+    })
+}
+
+pub fn set_reverse() -> FreeStaticPrimFuncVal {
+    FreeFn {
+        id: "set_reverse!",
+        f: free_impl(fn_set_reverse),
+        mode: free_mode(FuncMode::pair_mode(
+            Map::default(),
+            FuncMode::symbol_mode(SymbolMode::Literal),
+            FuncMode::default_mode(),
+        )),
+    }
+    .free_static()
+}
+
+fn fn_set_reverse(input: Val) -> Val {
+    let Val::Pair(pair) = input else {
+        error!("input {input:?} should be a pair");
+        return Val::default();
+    };
+    let pair = Pair::from(pair);
+    let Val::Symbol(id) = pair.first else {
+        error!("input.first {:?} should be a symbol", pair.first);
+        return Val::default();
+    };
+    let Val::Func(reverse) = pair.second else {
+        error!("input.second {:?} should be a function", pair.second);
+        return Val::default();
+    };
+    REVERSE_MAP.with(|map| {
+        let Ok(mut map) = map.try_borrow_mut() else {
+            error!("reverse map should be mutable");
+            return;
+        };
+        map.insert(id, reverse);
     });
     Val::default()
 }
