b6eb01b0d850c265cb0fc946eadddb7c91ee8c5d
LambdaAlpha <lambdaalpha@airlang.fun>
2025-12-15 19:00:09 +0800

    update key example
    Signed-off-by: LambdaAlpha <lambdaalpha@airlang.fun>

--- lib/src/syntax/test/text.rs
+++ lib/src/syntax/test/text.rs
@@ -4,7 +4,7 @@ use crate::syntax::test::text;
 pub(super) fn expected() -> Vec<Repr> {
     vec![
         text(""),
-        text(" ()[]{}<>\\|/'\":;!?,.`~@#$%^&*-+_="),
+        text("()[]{}<>\\|/'\".,:;!?_~-+=*^`@#$%&"),
         text("abc ABC 0123"),
         text("🜁🜂🜃🜄"),
         text("  \n\r\t\u{1f701}"),
@@ -13,7 +13,7 @@ pub(super) fn expected() -> Vec<Repr> {
         text("\u{1f701}"),
         text(" \t\r\n \t\r\n"),
         text(" \t\r\n\n \t\r\n"),
-        text("^_^t^r^n^_^t^r^n`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?\n"),
+        text("^_^t^r^n^_^t^r^n()[]{}<>\\|/'\".,:;!?_~-+=*^`@#$%&\n"),
         text(" \t\r\n \t\r\n^_^t^r^n^_^t^r^n \t\r\n \t\r\n"),
         text(" \t\r\n \t\r\n^_^t^r^n^_^t^r^n \t\r\n \t\r\n"),
         text(""),
